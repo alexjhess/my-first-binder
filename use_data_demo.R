1bc2277f3872ba7f57971a0c1915835c4677679f
alexjhess <48659784+alexjhess@users.noreply.github.com>
2024-04-23 11:17:05 +0200

    Create use_data_demo.R

---
+++ use_data_demo.R
@@ -0,0 +1,14 @@
+library(readr)
+library(tidyr)
+library(ggplot2)
+
+data <- read_csv("gapminder.csv") %>%
+    pivot_longer(-country,
+                 names_to = "year",
+                 values_to = "gdpPercap",
+                 names_prefix = "gdpPercap_",
+                 names_transform = list(year = as.integer))
+
+data[data$country == "Australia", ] %>%
+    ggplot(aes(x = year, y = gdpPercap)) +
+    geom_line()
